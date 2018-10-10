0888d6aaccbbfcba7b3fe2ea9d6fa5c07af23df8
SOURAV <souravthedifferent@gmail.com>
2018-10-10 15:54:16 +0530

    test

--- wap-consent-gateway/wcg-ui/wcg-servlet/src/main/java/com/ibm/wcg/controller/SubscriptionViewController.java
+++ wap-consent-gateway/wcg-ui/wcg-servlet/src/main/java/com/ibm/wcg/controller/SubscriptionViewController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-//test git
+//test git sourav
 
 @Controller
 public class SubscriptionViewController {
