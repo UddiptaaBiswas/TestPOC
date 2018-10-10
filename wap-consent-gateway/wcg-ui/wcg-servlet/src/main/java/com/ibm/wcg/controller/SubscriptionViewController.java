62392842d90f5e09541d95baf2069d0181c88de3
sunnapudiimmaniyelu <44023041+sunnapudiimmaniyelu@users.noreply.github.com>
2018-10-10 16:35:23 +0530

    test

--- wap-consent-gateway/wcg-ui/wcg-servlet/src/main/java/com/ibm/wcg/controller/SubscriptionViewController.java
+++ wap-consent-gateway/wcg-ui/wcg-servlet/src/main/java/com/ibm/wcg/controller/SubscriptionViewController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
+//test imani
 
 
 @Controller
